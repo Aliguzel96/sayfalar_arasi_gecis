a5d82a8a17d5613ba8224cf435facc338476cc32
example <email@example.com>
2020-11-04 16:59:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/multipleactivities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void git(View v){
+        Intent intent=new Intent(this,SecondPage.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/multipleactivities/SecondPage.java
@@ -0,0 +1,22 @@
+package com.example.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+    }
+    public void goMain(View v)
+    {
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+
+    }
+}
